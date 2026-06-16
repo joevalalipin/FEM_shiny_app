764055b55eff52187001179d67b3fd4f00994389
Joe Val Alipin <joe.valalipin@mpi.govt.nz>
2026-06-16 14:57:28 +1200

    chore: update ui text

--- ui.R
+++ ui.R
@@ -3,6 +3,10 @@ shinyUI(
     titlePanel(
       "FEM Shiny App"
       ),
+    titlePanel(
+    h5(HTML("This app is an R Shiny implementation of the <a href='https://github.com/Ministry-for-Primary-Industries/FarmEmissionsModel' target='_blank'>Farm Emissions R Model.</a>
+             User provides farm data on the input tab, and the output tab shows the summary emissions results. See Info tab for step-by-step instructions.<br>"))
+      ),
     sidebarPanel(
       width = 3,
       textInput("Entity_ID", "Entity_ID", value = "10002"),
@@ -77,9 +81,7 @@ shinyUI(
         
         tabPanel(
           "Info",
-          HTML("<br>This app is an R Shiny implementation of the <a href='https://github.com/Ministry-for-Primary-Industries/FarmEmissionsModel' target='_blank'>Farm Emissions R Model.</a><br>
-                      User provides farm data on the input tab, and the output tab shows the summary emissions results.<br>
-                      Please follow these steps.<br>
+          HTML("<br>Please follow these steps.<br>
                       1. Fill out the farm details on the left hand side. The fields are pre-populated with an example farm data.<br>
                       2. Provide the required data by filling out the tables on the input tab. *<br>
                       3. If more rows are required, right click on any cell and click 'Insert row'.<br>
@@ -87,12 +89,13 @@ shinyUI(
                       5. Click 'Download Data' to download results and input data. <br>
                       <br><br><br<br><br>
                       * <i>Tables are pre-populated with example data; you can remove all table contents by clicking 'Clear Tables' button at the bottom of the tab.<br>
+                      &nbsp;&nbsp;&nbsp;The model currently has limited validations, please ensure that your inputs are accurate.<br>
                       &nbsp;&nbsp;&nbsp;Columns have restrictions on what values you can enter:<br>
                       &nbsp;&nbsp;&nbsp;a. Stock count columns should be whole numbers.<br>
-                      &nbsp;&nbsp;&nbsp;b. Sectoral allocation of supplementary feed column should be between 1 and 0.<br>
-                      &nbsp;&nbsp;&nbsp;c. All numeric columns should be greater than or equal to 0.<br>
+                      &nbsp;&nbsp;&nbsp;b. Allocation columns should be between 1 and 0.<br>
+                      &nbsp;&nbsp;&nbsp;c. All numeric columns should be greater than or equal to 0 (except BreedingValues).<br>
                       &nbsp;&nbsp;&nbsp;d. Date inputs should follow the format 'YYYY-MM-DD'.<br>
-                      &nbsp;&nbsp;&nbsp;e. Date column should be within the reporting period, inclusive.</i><br>")
+                      &nbsp;&nbsp;&nbsp;e. Date column should be within the reporting period, inclusive (except EcoPond Treatment_Date).</i><br>")
           )
         )
       )
